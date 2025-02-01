9b1ad82851aa31093a19eb6e2a76e6042478fedf
YonanPrasetyo <yonanprasetyo91792@gmail.com>
2025-02-01 18:40:52 +0700

    feat: CRUD dasar hukum

--- app/Http/Controllers/DasarHukumController.php
+++ app/Http/Controllers/DasarHukumController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\DasarHukum;
+use Illuminate\Database\QueryException;
 
 class DasarHukumController extends Controller
 {
@@ -10,5 +12,55 @@ class DasarHukumController extends Controller
 
     public function index(){
         return view("pages.admin.dasar-hukum.dasar-hukum");
+    }
+
+    public function store(Request $request){
+        try{
+            $validateData = $request->validate([
+                'dasar_hukum' => 'required|string|max:255',
+            ]);
+
+            $dasarHukum = DasarHukum::create([
+                'dasar_hukum' => $validateData['dasar_hukum'],
+            ]);
+            return redirect()->back()->with('success', 'Dasar Hukum berhasil disimpan.');
+
+        }catch(\Exception $e){
+            return redirect()->back()->with('error', $e->getMessage());
+        }
+    }
+
+    public function update(Request $request, DasarHukum $dasarHukum){
+        try{
+            $validateData = $request->validate([
+                'dasar_hukum' => 'required|string|max:255',
+            ]);
+
+            $dasarHukum->update($validateData);
+            return redirect()->back()->with('success', 'Data berhasil diperbarui.');
+        } catch (QueryException $e) {
+            return redirect()->back()
+                ->withInput()
+                ->with('error', $e->getMessage());
+        } catch (\Exception $e) {
+            return redirect()->back()
+                ->withInput()
+                ->with('error', $e->getMessage());
+        }
+
+    }
+
+    public function destroy(DasarHukum $dasarHukum)
+    {
+        try {
+            $dasarHukum->delete();
+            return redirect()->back()->with('success', 'Data berhasil dihapus.');
+        } catch (QueryException $e) {
+            return redirect()->back()
+                ->with('error', $e->getMessage());
+        } catch (\Exception $e) {
+            return redirect()->back()
+                ->with('error', $e->getMessage());
+        }
     }
 }
