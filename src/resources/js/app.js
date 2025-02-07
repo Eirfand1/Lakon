95866de89287d05802e3ac2e0b78e2b536dd8569
Eirfand1 <name.fandi07@proton.me>
2025-02-07 22:00:55 +0700

    fix: darkmode & flatpickr state

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,19 +1,23 @@
 import { Livewire } from '../../vendor/livewire/livewire/dist/livewire.esm';
-
-Livewire.start()
+Livewire.start();
 
 import './bootstrap';
 import flatpickr from "flatpickr";
 
-
-document.addEventListener('livewire:navigated', () => {
-    // Light switcher
+function applyDarkMode() {
     const lightSwitches = document.querySelectorAll('.light-switch');
     if (lightSwitches.length > 0) {
         lightSwitches.forEach((lightSwitch, i) => {
             if (localStorage.getItem('dark-mode') === 'true') {
                 lightSwitch.checked = true;
+                document.documentElement.classList.add('dark');
+                document.querySelector('html').style.colorScheme = 'dark';
+            } else {
+                lightSwitch.checked = false;
+                document.documentElement.classList.remove('dark');
+                document.querySelector('html').style.colorScheme = 'light';
             }
+
             lightSwitch.addEventListener('change', () => {
                 const { checked } = lightSwitch;
                 lightSwitches.forEach((el, n) => {
@@ -21,8 +25,9 @@ document.addEventListener('livewire:navigated', () => {
                         el.checked = checked;
                     }
                 });
+
                 document.documentElement.classList.add('[&_*]:!transition-none');
-                if (lightSwitch.checked) {
+                if (checked) {
                     document.documentElement.classList.add('dark');
                     document.querySelector('html').style.colorScheme = 'dark';
                     localStorage.setItem('dark-mode', true);
@@ -33,13 +38,16 @@ document.addEventListener('livewire:navigated', () => {
                     localStorage.setItem('dark-mode', false);
                     document.dispatchEvent(new CustomEvent('darkMode', { detail: { mode: 'off' } }));
                 }
+
                 setTimeout(() => {
                     document.documentElement.classList.remove('[&_*]:!transition-none');
                 }, 1);
             });
         });
     }
-    // Flatpickr
+}
+
+function initializeFlatpickr() {
     flatpickr('.datepicker', {
         mode: 'range',
         static: true,
@@ -49,14 +57,20 @@ document.addEventListener('livewire:navigated', () => {
         prevArrow: '<svg class="fill-current" width="7" height="11" viewBox="0 0 7 11"><path d="M5.4 10.8l1.4-1.4-4-4 4-4L5.4 0 0 5.4z" /></svg>',
         nextArrow: '<svg class="fill-current" width="7" height="11" viewBox="0 0 7 11"><path d="M1.4 10.8L0 9.4l4-4-4-4L1.4 0l5.4 5.4z" /></svg>',
         onReady: (selectedDates, dateStr, instance) => {
-            // eslint-disable-next-line no-param-reassign
             instance.element.value = dateStr.replace('to', '-');
             const customClass = instance.element.getAttribute('data-class');
             instance.calendarContainer.classList.add(customClass);
         },
         onChange: (selectedDates, dateStr, instance) => {
-            // eslint-disable-next-line no-param-reassign
             instance.element.value = dateStr.replace('to', '-');
         },
     });
+}
+
+applyDarkMode();
+initializeFlatpickr();
+
+document.addEventListener('livewire:navigated', () => {
+    applyDarkMode();
+    initializeFlatpickr();
 });
