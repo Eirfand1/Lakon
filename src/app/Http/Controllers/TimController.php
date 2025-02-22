694a9a399a140bdfb32f3e5f3565e5a08988fc41
YonanPrasetyo <yonanprasetyo91792@gmail.com>
2025-02-22 18:56:24 +0700

    CRUD tim for lampiran kontrak

--- src/app/Http/Controllers/TimController.php
+++ src/app/Http/Controllers/TimController.php
@@ -23,7 +23,7 @@ class TimController extends Controller
                 'bulan_11' => $request->has('bulan_11'),
                 'bulan_12' => $request->has('bulan_12'),
             ]);
-            
+
             $validateData = $request->validate([
                 'kontrak_id' => 'required|exists:kontrak,kontrak_id',
                 'nama' => 'required|string|max:255',
@@ -43,27 +43,57 @@ class TimController extends Controller
                 'bulan_12' => 'boolean',
             ]);
 
-            $dasarHukum = Tim::create([
-                'kontrak_id' => $validateData['kontrak_id'],
-                'nama' => $validateData['nama'],
-                'posisi' => $validateData['posisi'],
-                'status_tenaga' => $validateData['status_tenaga'],
-                'bulan_1' => $validateData['bulan_1'],
-                'bulan_2' => $validateData['bulan_2'],
-                'bulan_3' => $validateData['bulan_3'],
-                'bulan_4' => $validateData['bulan_4'],
-                'bulan_5' => $validateData['bulan_5'],
-                'bulan_6' => $validateData['bulan_6'],
-                'bulan_7' => $validateData['bulan_7'],
-                'bulan_8' => $validateData['bulan_8'],
-                'bulan_9' => $validateData['bulan_9'],
-                'bulan_10' => $validateData['bulan_10'],
-                'bulan_11' => $validateData['bulan_11'],
-                'bulan_12' => $validateData['bulan_12'],
-            ]);
+            if ($request->tim_id) {
+                Tim::where('tim_id', $request->tim_id)->update([
+                    'kontrak_id' => $validateData['kontrak_id'],
+                    'nama' => $validateData['nama'],
+                    'posisi' => $validateData['posisi'],
+                    'status_tenaga' => $validateData['status_tenaga'],
+                    'bulan_1' => $validateData['bulan_1'],
+                    'bulan_2' => $validateData['bulan_2'],
+                    'bulan_3' => $validateData['bulan_3'],
+                    'bulan_4' => $validateData['bulan_4'],
+                    'bulan_5' => $validateData['bulan_5'],
+                    'bulan_6' => $validateData['bulan_6'],
+                    'bulan_7' => $validateData['bulan_7'],
+                    'bulan_8' => $validateData['bulan_8'],
+                    'bulan_9' => $validateData['bulan_9'],
+                    'bulan_10' => $validateData['bulan_10'],
+                    'bulan_11' => $validateData['bulan_11'],
+                    'bulan_12' => $validateData['bulan_12'],
+                ]);
+                return redirect()->back()->with('success', 'data berhasil diperbarui.');
+            }else {
+                Tim::create([
+                    'kontrak_id' => $validateData['kontrak_id'],
+                    'nama' => $validateData['nama'],
+                    'posisi' => $validateData['posisi'],
+                    'status_tenaga' => $validateData['status_tenaga'],
+                    'bulan_1' => $validateData['bulan_1'],
+                    'bulan_2' => $validateData['bulan_2'],
+                    'bulan_3' => $validateData['bulan_3'],
+                    'bulan_4' => $validateData['bulan_4'],
+                    'bulan_5' => $validateData['bulan_5'],
+                    'bulan_6' => $validateData['bulan_6'],
+                    'bulan_7' => $validateData['bulan_7'],
+                    'bulan_8' => $validateData['bulan_8'],
+                    'bulan_9' => $validateData['bulan_9'],
+                    'bulan_10' => $validateData['bulan_10'],
+                    'bulan_11' => $validateData['bulan_11'],
+                    'bulan_12' => $validateData['bulan_12'],
+                ]);
 
-            return redirect()->back()->with('success', 'Data Tim berhasil disimpan.');
+                return redirect()->back()->with('success', 'Data Tim berhasil disimpan.');
+            }
+        }catch(\Exception $e){
+            return redirect()->back()->with('error', $e->getMessage());
+        }
+    }
 
+    public function destroy($tim_id){
+        try{
+            Tim::where('tim_id', $tim_id)->delete();
+            return redirect()->back()->with('success', 'Data Tim berhasil dihapus.');
         }catch(\Exception $e){
             return redirect()->back()->with('error', $e->getMessage());
         }
