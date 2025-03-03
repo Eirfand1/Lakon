c9e0f4ef6561ce4249034426e6e52f24bf3bf2b5
YonanPrasetyo <yonanprasetyo91792@gmail.com>
2025-03-03 11:51:59 +0700

    fix realisasi penyedia

--- src/routes/web.php
+++ src/routes/web.php
@@ -76,8 +76,8 @@ Route::middleware(['auth', 'role:penyedia'])->prefix('/penyedia')->group(functio
 
     Route::middleware('cekStatusPenyedia:konsultan')->group(function () {
         Route::get('/matrik', [PenyediaController::class, 'konsultanMatrikIndex'])->name('penyedia.konsultan.matrik.index');
-        Route::get('/realisasi/{kontrak_id}', [PenyediaController::class, 'realisasi'])->name('penyedia.konsultan.realisasi');
-        Route::post('/realisasi/{kontrak_id}', [PenyediaController::class, 'storeRealisasi'])->name('penyedia.konsultan.realisasi.update');
+        Route::get('/realisasi/{kontrak_id}', [RealisasiController::class, 'realisasi'])->name('penyedia.konsultan.realisasi');
+        Route::post('/realisasi/{kontrak_id}', [RealisasiController::class, 'storeRealisasi'])->name('penyedia.konsultan.realisasi.update');
     });
 });
 
