2cfe9f74db902501e7c06c7b9bab750b14caf1e3
Eirfand1 <name.fandi07@proton.me>
2025-03-03 12:59:42 +0000

    fix realisasi datatable

--- src/app/Livewire/RealisasiTable.php
+++ src/app/Livewire/RealisasiTable.php
@@ -74,7 +74,7 @@ class RealisasiTable extends DataTableComponent
                                 <td class='p-2 border-b dark:border-gray-700'>{$realisasi->bulan}</td>
                                 <td class='p-2 border-b dark:border-gray-700'>{$realisasi->tahun}</td>
                                 <td class='p-2 border-b dark:border-gray-700'>{$realisasi->target}</td>
-                                <td class='p-2 dark:border-gray-700'>" . asset($realisasi->gambar) . "</td>
+                                <td class='p-2 dark:border-gray-700'><img src='" . asset($realisasi->gambar) . "' class='w-16 h-16 object-cover'></td>
                             </tr>
 
                         ";
