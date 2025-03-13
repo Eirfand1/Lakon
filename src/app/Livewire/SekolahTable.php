b6b8804a8bba3b3a7b7559b6c07b5037b58742fc
Eirfand1 <name.fandi07@proton.me>
2025-03-13 14:58:15 +0000

    nomor kontrak

--- src/app/Livewire/SekolahTable.php
+++ src/app/Livewire/SekolahTable.php
@@ -18,11 +18,12 @@ class SekolahTable extends DataTableComponent
     public function configure(): void
     {
         $this->setPrimaryKey('sekolah_id')
-             ->setFilterLayout('slide-down')
-             ->setPerPageAccepted([10,25,50,100, -1]);
+            ->setFilterLayout('slide-down')
+            ->setPerPageAccepted([10, 25, 50, 100, -1]);
     }
 
-    public function builder(): \Illuminate\Database\Eloquent\Builder{
+    public function builder(): \Illuminate\Database\Eloquent\Builder
+    {
         return Sekolah::query()
             ->selectRaw('*, ST_X(koordinat) as lat, ST_Y(koordinat) as lng')
             ->orderByDesc('updated_at');
@@ -39,7 +40,7 @@ class SekolahTable extends DataTableComponent
 
                     if ($lat === null || $lng === null) {
                         $row->koordinat = "-";
-                    }else {
+                    } else {
                         // gabungkan koordinat menjadi string
                         $row->koordinat = "$lat,$lng";
                     }
@@ -47,35 +48,43 @@ class SekolahTable extends DataTableComponent
                     return view('pages.admin.sekolah.actions', ['sekolah' => $row]);
                 }),
 
-            Column::make("NPSN","npsn")
+            Column::make("NPSN", "npsn")
                 ->searchable()
                 ->sortable(),
 
-            Column::make("Nama Sekolah","nama_sekolah")
+            Column::make("Nama Sekolah", "nama_sekolah")
                 ->searchable()
                 ->sortable(),
 
-            Column::make("Jenjang","jenjang")
+            Column::make("Jenjang", "jenjang")
                 ->searchable()
                 ->sortable(),
 
-            Column::make("Status","status")
+            Column::make("Status", "status")
                 ->searchable()
                 ->sortable(),
 
-            Column::make("Alamat","alamat")
+            Column::make('Kepala Sekolah', 'kepala_sekolah')
                 ->searchable()
                 ->sortable(),
 
-            Column::make("Desa","desa")
+            Column::make('NIP Kepsek', 'nip_kepala_sekolah')
                 ->searchable()
                 ->sortable(),
 
-            Column::make("Kecamatan","kecamatan")
+            Column::make("Alamat", "alamat")
                 ->searchable()
                 ->sortable(),
 
-            Column::make("Koordinat","koordinat")
+            Column::make("Desa", "desa")
+                ->searchable()
+                ->sortable(),
+
+            Column::make("Kecamatan", "kecamatan")
+                ->searchable()
+                ->sortable(),
+
+            Column::make("Koordinat", "koordinat")
                 ->format(function ($value, $row) {
                     // Ambil latitude dan longitude dari kolom koordinat
                     $lat = $row->lat;
@@ -84,7 +93,7 @@ class SekolahTable extends DataTableComponent
                     // Buat tautan ke Google Maps
                     if ($lat === null || $lng === null) {
                         $mapsUrl = "-";
-                    }else {
+                    } else {
                         $mapsUrl = "<a href='https://www.google.com/maps?q={$lat},{$lng}' target='_blank'>{$lat}, {$lng}</a>";
                     }
 
@@ -95,8 +104,8 @@ class SekolahTable extends DataTableComponent
                 ->searchable()
                 ->sortable(),
 
-            
-                // ->format(fn($value, $row) => view('pages.admin.sekolah.actions', ['sekolah' => $row])),
+
+            // ->format(fn($value, $row) => view('pages.admin.sekolah.actions', ['sekolah' => $row])),
         ];
     }
 
@@ -110,7 +119,7 @@ class SekolahTable extends DataTableComponent
                     'SD' => 'SD',
                     'SMP' => 'SMP',
                 ])
-                ->filter(function($builder, $value) {
+                ->filter(function ($builder, $value) {
                     return $value ? $builder->where('jenjang', $value) : $builder;
                 }),
 
@@ -120,7 +129,7 @@ class SekolahTable extends DataTableComponent
                     'SWASTA' => 'SWASTA',
                     'NEGERI' => 'NEGERI',
                 ])
-                ->filter(function($builder, $value) {
+                ->filter(function ($builder, $value) {
                     return $value ? $builder->where('status', $value) : $builder;
                 }),
 
@@ -128,7 +137,7 @@ class SekolahTable extends DataTableComponent
                 ->config([
                     'placeholder' => 'Cari Desa',
                 ])
-                ->filter(function($builder, $value) {
+                ->filter(function ($builder, $value) {
                     return $value ? $builder->where('desa', 'like', "%$value%") : $builder;
                 }),
 
@@ -136,7 +145,7 @@ class SekolahTable extends DataTableComponent
                 ->config([
                     'placeholder' => 'Cari Kecamatan',
                 ])
-                ->filter(function($builder, $value) {
+                ->filter(function ($builder, $value) {
                     return $value ? $builder->where('kecamatan', 'like', "%$value%") : $builder;
                 }),
         ];
