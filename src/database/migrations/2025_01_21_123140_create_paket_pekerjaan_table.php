609569ebc4d513ccda64b4fc5a3b6328ee65854a
Eirfand1 <name.fandi07@proton.me>
2025-04-13 12:49:45 +0000

    fix nomor spmk

--- src/database/migrations/2025_01_21_123140_create_paket_pekerjaan_table.php
+++ src/database/migrations/2025_01_21_123140_create_paket_pekerjaan_table.php
@@ -31,13 +31,13 @@ return new class extends Migration {
             $table->unsignedBigInteger('daskum_id');
             $table->foreign('daskum_id')
                 ->references('daskum_id')
-                ->on('dasar_hukum')
+                ->nullOnDelete()
                 ->onDelete('cascade');
             $table->integer('rup');
             $table->foreignId('sekolah_id')
                 ->references('sekolah_id')
                 ->on('sekolah')
-                ->onDelete('cascade')
+                ->nullOnDelete()
                 ->nullable();
             $table->timestamps();
             $table->softDeletes();
