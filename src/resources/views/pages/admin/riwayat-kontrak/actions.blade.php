5c22bbcb6eb687ea8647a4c6fa8e9b26bccc4607
Eirfand1 <name.fandi07@proton.me>
2025-04-18 14:41:02 +0000

    minor

--- src/resources/views/pages/admin/riwayat-kontrak/actions.blade.php
+++ src/resources/views/pages/admin/riwayat-kontrak/actions.blade.php
@@ -1,4 +1,4 @@
-<a href="{{route('verifikator.riwayat-kontrak.show', $kontrak)}}" class="btn rounded-md flex  btn-sm btn-warning text-white" wire:navigate>
+<a href="{{route('admin.riwayat-kontrak.show', $kontrak)}}" class="btn rounded-md flex  btn-sm btn-warning text-white" wire:navigate>
     <i class="fa fa-eye text-sm inline">
     </i>
 </a>
